ff7d34767bc9eb8db50620df1d6afc8dc1c9caee
IguanaBanana <ivanna.gomezmontes@gmail.com>
2024-02-18 21:01:29 -0700

    Subir  archivos por primera vez

---
+++ Extra/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return to_route('chirps.index')
+            ->with('status', __('Chirp created successfully!'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        $chirp->update($validated);
+
+        return to_route('chirps.index')
+            ->with('status', __('Chirp updated successfully!'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return to_route('chirps.index')
+            ->with('status', __('Chirp deleted successfully!'));
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# APPWEB_Extra
